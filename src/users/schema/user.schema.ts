60386de796a928137fa6d5722f9e4e1af991284e
leonardog9823 <guanuco43@gmail.com>
2023-05-06 05:02:18 -0300

    schema user

---
+++ src/users/schema/user.schema.ts
@@ -0,0 +1,57 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+//Return MongoDB documents
+export type UserDocument = HydratedDocument<User>;
+
+//No need to extend it from Document as we are
+//using HydratedDocument that already extends from Document
+@Schema()
+export class User {
+  @Prop({
+    type: String,
+    required: true,
+    trim: true,
+  })
+  name: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  })
+  email: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  password: string;
+
+  @Prop({
+    img: String,
+  })
+  img: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    enum: ['ADMIN_ROLE', 'USER_ROLE'],
+  })
+  rol: string;
+
+  @Prop({
+    type: Boolean,
+    default: true,
+  })
+  estado: boolean;
+
+  @Prop({
+    type: Boolean,
+    default: false,
+  })
+  google: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
