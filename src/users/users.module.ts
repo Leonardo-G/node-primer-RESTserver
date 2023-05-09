f02e6d0cc1371c2e12a9b72f884e9cd8c0d68f3e
leonardog9823 <guanuco43@gmail.com>
2023-05-08 23:01:27 -0300

    file controller and services

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schema/user.schema';
+import { UsersController } from './users/users.controller';
+import { UserController } from './controller/user.controller';
+import { UserService } from './services/user.service';
 
 @Module({
   imports: [
@@ -11,5 +14,7 @@ import { User, UserSchema } from './schema/user.schema';
       },
     ]),
   ],
+  controllers: [UsersController, UserController],
+  providers: [UserService],
 })
 export class UsersModule {}
